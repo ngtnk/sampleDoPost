5945d420a47467c9ec869c1f3cc0852c0623b31e
Yugo TANAKA <tanaka@neugate.co.jp>
2024-10-10 14:25:00 +0900

    Initial Commit

---
+++ src/main/java/controller/SampleServlet.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SampleServlet
+ */
+@WebServlet("/SampleServlet")
+public class SampleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		HttpSession session = request.getSession();
+		String hoge = request.getParameter("hoge");
+		String fuga = request.getParameter("fuga");
+		System.out.println("hoge: " + hoge);
+		System.out.println("fuga: " + fuga);
+	}
+
+}
---
+++ src/main/webapp/sample.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="SampleServlet?hoge=1" method="post">
+		<input type="text" name="fuga">
+		<input type="text" name = "">
+		
+		<input type="submit">
+	</form>
+</body>
+</html>
